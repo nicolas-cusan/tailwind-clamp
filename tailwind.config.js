c2cd4f8e396f8f60d33b6123e00b33ed67048366
Nicolas Cusan <nicolas-cusan@users.noreply.github.com>
2025-01-15 14:00:55 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ export default {
   theme: {
     extend: {
       padding: { test: clampValue(30, 50) },
+      fontSize: {
+        size1: ['20rem', { lineHeight: '22rem', letterSpacing: '0rem' }],
+        size2: ['24rem', { lineHeight: '28rem', letterSpacing: '0rem' }],
+      },
     },
   },
   plugins: [tailwindClamp(options)],
